7497aa0ab8cb3a4b090f3d22956c580f790f8283
pusliaMary <pusliaprus@gmail.com>
2025-11-27 22:22:54 +0300

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,15 @@ import { Cart } from './Components/Cart/Cart'
 import { AllCategories } from './Components/Filter/AllCategories'
 import { Dishes } from './Components/DishesComponents/Dishes'
 import { useState, useEffect } from 'react'
-import { LoaderPage } from './Loader/LoaderPage'
+
 
 
 function App() {
   
-  const [loader, setLoader] = useState(true)
 
-  useEffect(()=> {
-    const timer = setTimeout(()=> setLoader(false), 2000)
-    return ()=> clearTimeout(timer)
-  }, [])
 
   return (
    <div className="App">
-      {loader && <LoaderPage />}
       <div className='block columnCenter'>
         <div className="header">
             <h1>Choose your preferences...</h1>
